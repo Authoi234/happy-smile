b7a883a47e61e3ac5f08065ae9dbbe197448cc9f
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-24 14:44:10 +0630

    single service details is creating

--- src/Pages/Home/Services/SingleServiceDetails.js
+++ src/Pages/Home/Services/SingleServiceDetails.js
@@ -5,13 +5,15 @@ const SingleServiceDetails = () => {
     const service = useLoaderData();
     console.log(service);
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    Button
+        <div className='py-20 bg-no-repeat bg-cover block justify-center items-center' style={{backgroundImage: `url(${service.img})`, backgroundPosition: 'center'}}>
+            <div className="card bg-transparent backdrop-blur-sm glass mx-72">
+                <figure><img className='w-full' src={service.img} alt="" /></figure>
+                <div className="card-body">
+                    <h2 className="text-4xl font-bold text-white">{service.name}</h2>
+                    <p className='text-2xl text-white'>{service.details}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Learn now!</button>
+                    </div>
                 </div>
             </div>
         </div>
